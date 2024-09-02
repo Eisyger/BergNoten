219f0f5fc96968724eb9f96fc9ef85314715035e
Eisyger <140314733+Eisyger@users.noreply.github.com>
2024-09-02 19:29:41 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BergNoten
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
